245ea5ef9adf9e3f0546e0ca5bc706df8232461f
CBalthazar <balthclement@gmail.com>
2024-12-04 16:45:59 +0100

    push return f-fing length tss

--- script.js
+++ script.js
@@ -5,16 +5,19 @@ const MOCKED_DATA = [
   { id: 4, name: "douche", completed: false },
 ];
 
-async function mock_fetch(ms, path = false) {
+async function mock_fetch(ms) {
   await new Promise((resolve) => setTimeout(resolve, ms));
   storeData(MOCKED_DATA);
+
   return MOCKED_DATA;
 }
 
 function storeData(data) {
   let stored = JSON.parse(localStorage.getItem("todos")) || [];
-  localStorage.setItem("todos", JSON.stringify(stored.push(...data)));
+  stored.push(...data);
+  localStorage.setItem("todos", JSON.stringify(stored));
 }
 
 // Main
 mock_fetch(Math.random() * 2000 + 2000);
+setTimeout(localStorage.clear, 10000);
